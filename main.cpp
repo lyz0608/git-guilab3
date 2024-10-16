32700838c2537008d6f1b51bf298fa8df34df667
Student <1291573216@qq.com>
2024-10-16 14:08:43 +0300

    added greeting Git info

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ int main(){
     char name[20];
     cout<<"Enter your name:";
     cin>>name;
-    cout<<"Hello,"<<name<<"!\n";
+    cout<<"Hello,<<name[D[D[D[D[D[D"<<name<<"!\n";
+    cout<<"Nice to see you learning Git\n";
     system("pause");
     return 0;
 }
